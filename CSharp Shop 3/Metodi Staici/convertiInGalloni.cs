6e1bf90b464cfb9de03abf16d906b61b7f7a208b
DrLiveasy <youssef.saleh.hans@gmail.com>
2022-12-06 15:54:24 +0100

    classe statica con un metodo statcio che converte litri in galloni

---
+++ CSharp Shop 3/Metodi Staici/convertiInGalloni.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Shop_3.Metodi_Staici
+{
+    public static class convertiInGalloni
+    {
+        public static readonly double GalloneALitro = 3.785;
+
+        public static double convertiLitriInGalloni(string litriDaConvertire)
+        {
+            
+            double Litri = double.Parse(litriDaConvertire);
+            double Galloni = Litri * GalloneALitro;
+
+            return Galloni;
+
+        }
+    }
+}
